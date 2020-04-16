91db71908d18c16a9c3bbaa7324a3522a35acf4f
arturo cruz <acruz.nok@gmail.com>
2020-04-16 12:31:30 -0500

    Refactor a la view para mostrar las sesiones

--- calendario/sesiones/views.py
+++ calendario/sesiones/views.py
@@ -9,39 +9,32 @@ from .models import Session, Calendar, CalendarSession
 from .forms import AddSessionForm
 
 def calendarioFundamentos(request):
-    context = {}
     sessions = CalendarSession.objects.filter(calendar=1)
-    
+    context = {
+        'group': "Fundamentos de programación",
+        'idGroup': 1,
+    }
     if sessions:
-        context = {
-            'empty': 0,
-            'group': "Fundamentos de programación",
-            'idGroup': 1,
-            'sessions': sessions
-        }
-        return render(request, 'sesiones/index.html', context)
+        context.update([ ('empty', 0) , ('sessions', sessions) ])
     # Si la cantidad de sesiones es 0
     else:
-        context = {
-            'empty': 1,
-            'group': "Fundamentos de programación",
-            'idGroup': 1
-        }
-        return render(request, 'sesiones/index.html', context)
-    
+        context.update([ ('empty', 1) ])
+    print(context)
+    return render(request, 'sesiones/index.html', context)
+
 def addSessionGroup(request):
     form = AddSessionForm()
-    
-    
+
+
     context = {
         'form': form
     }
-    
+
     # s = Session()
     # s.name = request.POST.get('name')
     # s.body = request.POST.get('body')
     # s.type = sessionType
     # s.save()
     # if(request.POST.get('type') == 0): sessionType = True
-    
+
     return render(request, 'sesiones/forms/agregar.html', context)
